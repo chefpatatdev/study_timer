1ac6009eff9539c551160dec9909e8973a71dc02
chefpatat <dasilvamarquesricky@gmail.com>
2023-05-10 14:28:04 +0200

    auto start stop

--- GUI_layer/Form1.cs
+++ GUI_layer/Form1.cs
@@ -18,6 +18,7 @@ namespace GUI_layer
 
         public Settings settingsApp;
         public Statistics statisticsApp;
+        
 
         public GUI_studytimer()
         {
@@ -26,24 +27,12 @@ namespace GUI_layer
             timedisplay.Text = currentTime.ToString(@"hh\:mm\:ss");
             settingsApp = new Settings();
             statisticsApp = new Statistics();
-            
 
         }
         private void startstop_Click(object sender, EventArgs e)
         {
-            if (running)
-            {
-                clock.Stop();
-                startstop.Text = "start";
-                this.BackColor = Color.Green;
-            }
-            else
-            {
-                clock.Start();
-                startstop.Text = "stop";
-                this.BackColor = Color.Red;
-            }
             running = !running;
+            updateScreen();
         }
         private void clock_Tick(object sender, System.EventArgs e)
         {
@@ -51,21 +40,19 @@ namespace GUI_layer
             timedisplay.Text = currentTime.ToString(@"hh\:mm\:ss");
             if (currentTime == TimeSpan.Zero)
             {
-                if (settingsApp.AutoStartStudy)
-                {
-
-                }
                 if (mode == "study")
                 {
+                    running = settingsApp.AutoStartBreak;
                     mode = "pause";
+                    updateScreen();
                     currentTime = new TimeSpan(pausetime.Value.Hour, pausetime.Value.Minute, pausetime.Value.Second);
-                    this.BackColor = Color.Blue;
                 }
                 else
                 {
+                    running = settingsApp.AutoStartStudy;
                     mode = "study";
+                    updateScreen();
                     currentTime = new TimeSpan(studytime.Value.Hour, studytime.Value.Minute, studytime.Value.Second);
-                    this.BackColor = Color.Red;
                 }
                 timedisplay.Text = currentTime.ToString(@"hh\:mm\:ss");
             }
@@ -87,6 +74,42 @@ namespace GUI_layer
             timedisplay.Text = currentTime.ToString(@"hh\:mm\:ss");
         }
 
+        private void settingsmenu_Enter(object sender, EventArgs e)
+        {
 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            settingsApp.AutoStartBreak = AUTO_START_BREAK_CHECK.Checked;
+            Console.WriteLine(settingsApp.AutoStartBreak);
+        }
+
+        private void AUTO_START_STUDY_CHECKBOX_CheckedChanged(object sender, EventArgs e)
+        {
+            settingsApp.AutoStartStudy = AUTO_START_STUDY_CHECKBOX.Checked;
+        }
+
+        private void updateScreen()
+        {
+            if (mode == "study")
+            {
+                this.BackColor = Color.Red;
+            }
+            else if (mode == "pause")
+            {
+                this.BackColor = Color.Blue;
+            }
+            if (!running)
+            {
+                clock.Stop();
+                startstop.Text = "start";
+            }
+            else
+            {
+                clock.Start();
+                startstop.Text = "stop";
+            }
+        }
     }
 }
